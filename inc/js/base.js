bf2157a2b399f0b74a8acfa6b5026e6a1399fd6d
Yamil Burgos <yamil.burgos1293@gmail.com>
2017-07-29 12:12:51 -0400

    Updated site with new javascript & css

--- inc/js/base.js
+++ inc/js/base.js
@@ -1,41 +1,58 @@
 "use strict";
 
-addEventListener();
-
-function addEventListener() {
-	 document.querySelector(".carousel-next").addEventListener("click", function() {
-        goForward();
-     });
+class slideController {
+	constructor() {
+		this.slideIndex = 1;
+		this.slideList = document.querySelectorAll("article");
+		this.addEventListener();
+	}
 	
-	document.querySelector(".carousel-prev").addEventListener("click", function() {
-        goBack();
-     });
-}
-
-var slideIndex = 1;
-
-function goForward() {
-  showSlides(slideIndex += 1);
-}
-
-function goBack() {
-  showSlides(slideIndex -= 1);
-}
-
-function showSlides(n) {
-  	var slides = document.querySelectorAll("article");
-
-	if (n > slides.length) {
-		slideIndex = 1;
-	} 
-  	
-	if (n < 1) {
-		slideIndex = slides.length;
+	addEventListener() {
+		document.querySelector(".carousel-next").addEventListener("click", () => {
+			this.moveSlideOut(false);
+			
+			setTimeout(() => {
+				this.showSlides(this.slideIndex += 1, false);
+			}, 300)
+     	});
+	
+		document.querySelector(".carousel-prev").addEventListener("click", () => {
+			this.moveSlideOut(true);
+			
+			setTimeout(() => {
+				this.showSlides(this.slideIndex -= 1, true);
+			}, 300)
+     	});
 	}
-  
-	for (var i = 0; i < slides.length; i++) {
-      	slides[i].style.display = "none";
-	}
+	
+	showSlides(currentSlideNumber, movingLeft) {		
+		if (currentSlideNumber > this.slideList.length) {
+			this.slideIndex = 1;
+		} 
+
+		if (currentSlideNumber < 1) {
+			this.slideIndex = this.slideList.length;
+		}
 		
-  	slides[slideIndex-1].style.display = "block";
+		for (var i = 0; i < this.slideList.length; i++) {	
+			this.slideList[i].style.display = "none";
+		}
+		
+		this.slideList[this.slideIndex - 1].style.display = "block";	
+		this.moveSlideIn(movingLeft);
+	}
+	
+	moveSlideOut(movingLeft) {
+		this.slideList[this.slideIndex - 1].style = (movingLeft === false) 
+			? "animation-name: slideRightOut; animation-duration: 1s;"
+			: "animation-name: slideLeftOut; animation-duration: 1s;";
+	}
+	
+	moveSlideIn(movingLeft) {
+		this.slideList[this.slideIndex - 1].style = (movingLeft === false) 
+			? "animation-name: slideRightIn; animation-duration: 1s;"
+			: "animation-name: slideLeftIn; animation-duration: 1s;";
+	}
 }
+
+new slideController();
